aa9307df93e3516504691e9e90c4db8ecb1e9a44
WSengR <844537802@qq.com>
2016-11-03 11:58:27 +0800

    控件封装及适配处理

--- app/src/main/java/com/example/wsr/myapplication/activity/TestPageActivity.java
+++ app/src/main/java/com/example/wsr/myapplication/activity/TestPageActivity.java
@@ -10,6 +10,7 @@ import com.example.wsr.myapplication.bean.RedBean;
 import com.example.wsr.myapplication.quest.BaseQuestStart;
 import com.sunrun.sunrunframwork.bean.BaseBean;
 import com.sunrun.sunrunframwork.uibase.PagingActivity;
+import com.sunrun.sunrunframwork.view.title.BaseTitleLayoutView;
 import com.sunrun.sunrunframwork.weight.pulltorefresh.PullToRefreshBase;
 import com.sunrun.sunrunframwork.weight.pulltorefresh.PullToRefreshListView;
 
@@ -29,6 +30,8 @@ public class TestPageActivity extends PagingActivity {
     @Bind(R.id.prlv_test)
     PullToRefreshListView prlvTest;
     int curPage = 0;
+    @Bind(R.id.title_layout_view)
+    BaseTitleLayoutView titleLayoutView;
 
     @Override
     protected void initView() {
@@ -50,7 +53,7 @@ public class TestPageActivity extends PagingActivity {
         super.nofityUpdate(requestCode, bean);
         //必须设置这个 内有ListView与Adapter的方法
         setDataToView(RedBean.getData().getInfo().getLable_result(), prlvTest.getRefreshableView());
-
+        titleLayoutView.setTitleText("CuurPage" + curPage);
         Log.e("wsr", "curPage = " + curPage);
     }
 
@@ -61,4 +64,10 @@ public class TestPageActivity extends PagingActivity {
 
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
 }
